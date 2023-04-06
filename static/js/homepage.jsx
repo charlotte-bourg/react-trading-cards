1425c725621f4cfad3e86ae1a689e4ceaeccb4ca
Charlotte <charlotte.bourg@gmail.com>
2023-04-06 12:30:04 -0700

    main portion of lab

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return(
+    <div><p>Welcome to the trading card site!</p>
+    <p><a href= "/cards">Cards page</a></p>
+    <img src="/static/img/balloonicorn.jpg"></img>
+    </div>
+
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
